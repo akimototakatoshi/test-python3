bf467543775e147b08c9b07003c6392da74cfc16
秋元崇利 <takatoshi.akimoto@rakus-pertners.co.jp>
2023-10-30 15:51:23 +0900

    add: 削除機能の追加

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ import requests
 import json
 import pandas as pd
 
-page = st.sidebar.selectbox("Choose your page", ["users", "rooms", "bookings"])
+page = st.sidebar.selectbox("Choose your page", ["ユーザー登録", "会議室登録", "予約登録", "登録取り消し"])
 
-if page == "users":
+if page == "ユーザー登録":
     st.title("ユーザー登録画面")
     with st.form(key="user"):
         # user_id: int = random.randint(0, 10)
         username: str = st.text_input("ユーザー名", max_chars=12)
         data = {
-            # "user_id": user_id,
             "username": username
         }
         submit_button = st.form_submit_button(label="ユーザー登録")
@@ -26,11 +25,11 @@ if page == "users":
         if res.status_code == 200:
             st.success("ユーザー登録完了")
 
-elif page == "rooms":
+elif page == "会議室登録":
     st.title("会議室登録画面")
 
     with st.form(key="room"):
-        room_id: int = random.randint(0, 10)
+        # room_id: int = random.randint(0, 10)
         room_name: str = st.text_input("会議室名", max_chars=12)
         capacity: int = st.number_input("定員", step=1)
         data = {
@@ -48,18 +47,19 @@ elif page == "rooms":
         )
         if res.status_code == 200:
             st.success("会議室登録完了")
-        st.json(res.json())
 
-elif page == "bookings":
+elif page == "予約登録":
     st.title("会議室予約画面")
     # ユーザー一覧取得
     url_users = "http://127.0.0.1:8000/users"
     res = requests.get(url_users)
     users = res.json()
+
     # ユーザー名をキー、ユーザーIDをバリュー
     users_name = {}
     for user in users:
         users_name[user["username"]] = user["user_id"]
+    
 
     #会議室一覧の取得
     url_rooms = "http://127.0.0.1:8000/rooms"
@@ -83,10 +83,12 @@ elif page == "bookings":
     bookings = res.json()
     df_bookings = pd.DataFrame(bookings)
 
+    #ユーザー一覧の表示
     users_id = {}
     for user in users:
         users_id[user["user_id"]] = user["username"]
 
+    #会議室一覧の表示
     rooms_id = {}
     for room in rooms:
         rooms_id[room["room_id"]] = {
@@ -99,12 +101,13 @@ elif page == "bookings":
     to_room_name = lambda x: rooms_id[x]["room_name"]
     to_datetime = lambda x: datetime.datetime.fromisoformat(x).strftime("%Y/%M/%d %H:%M")
 
+
     #特定の列に適用
     df_bookings["user_id"] = df_bookings["user_id"].map(to_username)
     df_bookings["room_id"] = df_bookings["room_id"].map(to_room_name)
     df_bookings["start_datetime"] = df_bookings["start_datetime"].map(to_datetime)
     df_bookings["end_datetime"] = df_bookings["end_datetime"].map(to_datetime)
-
+    print(df_bookings)
     df_bookings = df_bookings.rename(columns={
         "user_id": "予約者名",
         "room_id": "会議室名",
@@ -116,7 +119,6 @@ elif page == "bookings":
     
     st.write("###  予約一覧")
     st.table(df_bookings)
-
 
     with st.form(key="booking"):
         # booking_id: int = random.randint(0, 10)
@@ -132,7 +134,6 @@ elif page == "bookings":
         user_id: int = users_name[username]
         room_id: int = rooms_name[room_name]["room_id"]
         capacity: int = rooms_name[room_name]["capacity"]
-
 
         data = {
             "user_id": user_id,
@@ -157,7 +158,7 @@ elif page == "bookings":
         # 定員以上の予約人数の場合
         if booked_num > capacity:
             st.error(f"{room_name}の定員は、{capacity}名です。{capacity}名以下の予約人数のみ受け付けます。")
-         # 開始時刻より終了時奥の方が多い場合   
+        # 開始時刻より終了時奥の方が多い場合   
         elif start_time >= end_time:
             st.error("開始時刻が終了時刻を超えています。") 
         #開始時刻(9時)~終了時刻(20時)の間に収まっていない場合
@@ -175,3 +176,22 @@ elif page == "bookings":
             elif res.status_code == 404 and res.json()["detail"] == "Already booked":
                 st.error("指定の時間にはすでに予約が入っています")
 
+elif page == "登録取り消し":
+    st.markdown("<h1 style='color: red;'>予約取り消し画面</h1>", unsafe_allow_html=True)
+
+    user_id: int = st.text_input("ユーザーIDを入力")
+    data = {
+        "user_id": user_id 
+    }
+    delete_button = st.button(label="ユーザー取り消し")
+
+    if delete_button:
+        url = "http://127.0.0.1:8000/users/{user_id}"
+        res = requests.delete(
+            url,
+            data=json.dumps(data)
+        )
+        if res.status_code == 200:
+            st.success("ユーザー取り消し完了")
+        elif res.status_code == 404 or res.status_code == 422:
+            st.error("取り消しに失敗しました。")
